28636d910e7c159bf82b2b1b8d1d7f051f58981d
jianjun2015 <jianjun_info@163.com>
2018-03-21 18:51:08 +0800

    block'chain

--- src/main/java/com/blockchain/model/TransactionOutput.java
+++ src/main/java/com/blockchain/model/TransactionOutput.java
@@ -8,4 +8,38 @@ package com.blockchain.model;
  * @version:
  */
 public class TransactionOutput {
+
+    /**
+     * 交易金额
+     */
+    private int value;
+    /**
+     * 交易接收方的钱包公钥的hash值
+     */
+    private String publicKeyHash;
+
+    public TransactionOutput() {
+        super();
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getPublicKeyHash() {
+        return publicKeyHash;
+    }
+
+    public void setPublicKeyHash(String publicKeyHash) {
+        this.publicKeyHash = publicKeyHash;
+    }
+
+    public TransactionOutput(int value, String publicKeyHash) {
+        this.value = value;
+        this.publicKeyHash = publicKeyHash;
+    }
 }
